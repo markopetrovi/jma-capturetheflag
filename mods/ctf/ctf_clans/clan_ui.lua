ee0661f4f21749fbb4722d001bc890a67e728e20
Nanowolf4 <n4w@tutanota.com>
2024-08-25 18:09:17 +0300

    Add ctf_clans (work-in-progress)

---
+++ mods/ctf/ctf_clans/clan_ui.lua
@@ -0,0 +1,416 @@
+local ui_data = {}
+
+local back_to_main_button = "button[9.43,0.035;1,1;back_to_main;X]"
+
+local function get_info_form(msg)
+	return "hypertext[0,1;10.47,1.85;h;<global valign=middle> <center>" .. msg .. "</center>]"
+end
+
+local formspecs = {
+	info_form = function(player, ui_ctx)
+		local info = ui_ctx.info
+		local msg = info.msg
+		local status = info.status
+
+		local img = "ctf_clans_question.png"
+		if status == true then
+			img = "ctf_clans_check.png"
+		else
+			img = "ctf_clans_x.png"
+		end
+
+		if info.color then
+			msg = "<style color=" .. info.color .. ">" .. msg .. "</style>"
+		end
+
+		if info.bold then
+			msg = "<b>" .. msg .. "</b>"
+		end
+
+		ui_ctx.info = nil
+		return back_to_main_button
+			.. get_info_form(msg)
+			.. "image[4.235,4.5;2,2;" .. img .. "]"
+	end,
+
+	question_form = function(player, ui_ctx)
+		local question = ui_ctx.question
+		local msg =  question.msg
+
+		if question.bold then
+			msg = "<b>" .. msg .. "</b>"
+		end
+
+		return get_info_form(msg)
+			.. "button[5.735,8;2,1;question_yes;Yes]"
+			.. "button[2.735,8;2,1;question_no;No]"
+			.. "image[4.235,4.5;2,2;ctf_clans_question.png]"
+	end,
+
+	no_clan = function(player, ui_ctx)
+		local no_clan_ht = [[
+			<global valign=middle>
+			<center><b>You are currently not a member of any clan.</b>
+			You have the option to start your own clan or join an existing one by accepting an invitation.</center>
+		]]
+		return back_to_main_button
+			.. "hypertext[0,0;10.47,11.35;h;" .. no_clan_ht .. "]"
+			.. "button[3.73,7.235;3,1;new_clan;I want a new clan]"
+	end,
+
+	invite = function(player, ui_ctx)
+		local msg = "<b>Enter the name of the player you would like to invite</b>"
+		local elems = "button[7.03,4.5;2,1;invite_send;Send Invite]"
+			.. "field[2,4.5;5,1;invite_playername;Player Name;" .. (ui_ctx.invite_field or "") .. "]"
+
+		if ui_ctx.invite_error then
+			msg = "<style color=red><b>" .. ui_ctx.invite_error .. "</b></style>"
+			ui_ctx.invite_error = nil
+		end
+
+		return back_to_main_button .. get_info_form(msg) .. elems
+	end,
+
+	clan_manager = function(player, ui_ctx, id)
+		local this_clan = ctf_clans.get_clan_def(id)
+		if not this_clan then return end
+
+		ui_ctx.page = "clan_manager"
+		local list = ""
+		local first = true
+		local table_imgs = {}
+		ui_ctx.playerlist = {}
+		local icon_i = 0
+
+		for pn, def in pairs(this_clan.members) do
+			table.insert(ui_ctx.playerlist, pn)
+			local rank_name = def.rank
+
+			if rank_name ~= "member" then
+				pn = string.format("%s [%s]", pn, rank_name)
+			end
+
+			local icon = ctf_clans.get_rank_icon(this_clan.ranks[rank_name])
+			local col_img
+			if icon then
+				if table_imgs[icon] then
+					col_img = table_imgs[icon]
+				else
+					icon_i = icon_i + 1
+					table_imgs[icon] = icon_i
+					col_img = icon_i
+				end
+			end
+
+			if first then
+				first = false
+			else
+				list = list .. ","
+			end
+
+			list = list .. (col_img or "") .. "," .. minetest.formspec_escape(pn)
+
+		end
+
+		local column_imgs = "image"
+		for fn, i in pairs(table_imgs) do
+			column_imgs = column_imgs .. "," .. string.format("%d=%s", i, fn)
+		end
+		local ht = "hypertext[0.1,0.1;10.3,1;h;<global valign=middle color=" .. this_clan.color .."><big>" .. this_clan.clan_name
+			.. "<img name=default_dirt.png width=25 height=25 float=left>]"
+
+		local formspec = "box[5.6,0.9;4.8,10.4;gray]"
+			.. "box[0.1,0.1;10.3,1;black]"
+			.. ht
+			.. "hypertext[0.1,1.1;5.2,5;h;<center><b>Description</b></center>\n" .. (this_clan.description or "?") .. "]"
+			.. "tablecolumns[" .. column_imgs .. ";text]"
+			.. "tableoptions[highlight=#777777]"
+			.. "table[5.9,1.4;4.3,7.4;playerlist;" .. list .. "]"
+			.. "button[2.3,10;2.5,0.8;change_color;Change Color]"
+
+
+		local player_name = player:get_player_name()
+
+		if ctf_clans.has_permission(id, player_name, "invite") then
+			formspec = formspec .. "button[8.3,0.2;2,0.8;invite;Invite]"
+		end
+
+		if ui_ctx.playerlist_selected and ui_ctx.playerlist_selected ~= player_name then
+			if ctf_clans.has_permission(id, player_name, "kick") then
+				formspec = formspec .. "button[8.2,9;2,0.8;kick;Kick]"
+			end
+
+			if ctf_clans.has_permission(id, player_name, "set_rank") then
+				formspec = formspec .. "button[5.9,9;2,0.8;set_rank;Set Rank]"
+			end
+		end
+
+		return formspec
+	end,
+
+	set_rank = function(player, ui_ctx, id)
+		ui_ctx.ranklist = {}
+		local this_clan = ctf_clans.get_clan_def(id)
+		local list = ""
+		local table_imgs = {}
+		local first = true
+		local icon_i = 0
+		for rank_name, def in pairs(this_clan.ranks) do
+			table.insert(ui_ctx.ranklist, rank_name)
+			local icon = ctf_clans.get_rank_icon(def)
+			local col_img
+			if icon then
+				if table_imgs[icon] then
+					col_img = table_imgs[icon]
+				else
+					icon_i = icon_i + 1
+					table_imgs[icon] = icon_i
+					col_img = icon_i
+				end
+			end
+
+			if first then
+				first = false
+			else
+				list = list .. ","
+			end
+
+			list = list .. (col_img or "") .. "," .. minetest.formspec_escape(rank_name)
+		end
+
+		local column_imgs = "image"
+		for fn, i in pairs(table_imgs) do
+			column_imgs = column_imgs .. "," .. string.format("%d=%s", i, fn)
+		end
+
+		local msg = "Select a rank"
+		if ui_ctx.set_rank_error then
+			msg = "<style color=red><b>" .. ui_ctx.set_rank_error .. "</b></style>"
+			ui_ctx.set_rank_error = nil
+		end
+
+		return back_to_main_button
+			.. get_info_form(msg)
+			.. "button[4.235,8;2,1;set_rank_apply;Apply]"
+			.. "tablecolumns[".. column_imgs .. ";text]"
+			.. "table[0.5,3;9.5,4.5;ranklist;" .. list .. "]"
+	end
+}
+
+sfinv.register_page("sfinv:clans", {
+	title = ("Clans"),
+	get = function(self, player, context)
+		local player_name = player:get_player_name()
+		local ctx = ui_data[player_name]
+		local id = ctf_clans.get_clan_id(player_name)
+		local fs = ""
+		if ctx.page and formspecs[ctx.page] then
+			fs = formspecs[ctx.page](player, ctx, id)
+		else
+			if id then
+				fs = formspecs.clan_manager(player, ctx, id)
+			else
+				fs = formspecs.no_clan(player, ctx)
+			end
+		end
+
+		return sfinv.make_formspec_v7(player, context, fs, false)
+	end,
+
+	on_player_receive_fields = function(self, player, context, fields)
+		local player_name = player:get_player_name()
+		local ctx = ui_data[player_name]
+		local id = ctf_clans.get_clan_id(player_name)
+		if not ctf_clans.is_clan_exist(id) then
+			if fields.new_clan then
+				ctf_clans.show_clan_creator(player_name)
+			end
+			ctx.page = "no_clan"
+			return true, true
+		end
+
+		local clan_def = ctf_clans.get_clan_def(id)
+		local update_fs = false
+
+		if fields.back_to_main then
+			ctx.page = "clan_manager"
+			ctx.invite_field = nil
+			update_fs = true
+
+		elseif fields.invite then
+			ctx.page = "invite"
+			update_fs = true
+
+		elseif fields.invite_send then
+			if not ctf_clans.has_permission(id, player_name, "invite") then
+				ctx.page = "info_form"
+				ctx.info = {
+					msg = "Insufficient privileges",
+					status = true,
+					color = "red",
+					bold = true
+				}
+				return true, true
+			end
+
+			local target = fields.invite_playername:trim()
+			if target:match("^%s*$") == nil then
+				ctx.invite_field = target
+				if target ~= player_name then
+					if minetest.player_exists(target) then
+						if ctf_clans.get_clan_id(target) then
+							ctx.invite_error = "This player is already a member of another clan, you cannot invite them"
+						else
+							ctf_clans.invite_player(target, player_name, id)
+
+							ctx.page = "info_form"
+							ctx.info = {
+								msg = "An invitation has been sent to " .. target,
+								status = true,
+								bold = true,
+								color = "lime"
+							}
+						end
+					else
+						ctx.invite_error = "This player does not exist"
+					end
+				else
+					ctx.invite_errorr_msg = "Are you serious? Good luck, hacker xD"
+				end
+			else
+				ctx.invite_error = "Player name not provided"
+			end
+
+			update_fs = true
+
+		elseif fields.playerlist then
+			local event = minetest.explode_table_event(fields.playerlist)
+			local player_index = event.row
+			if ctx.playerlist[player_index] then
+				ctx.playerlist_selected = ctx.playerlist[player_index]
+				update_fs = true
+			end
+
+		elseif fields.set_rank then
+			if ctx.playerlist_selected then
+				ctx.page = "set_rank"
+			end
+			update_fs = true
+
+		elseif fields.ranklist then
+			local event = minetest.explode_table_event(fields.ranklist)
+			local rank_index = event.row
+			if ctx.ranklist[rank_index] then
+				ctx.rank_selected = rank_index
+				update_fs = true
+			end
+
+		elseif fields.set_rank_apply then
+			if not ctf_clans.has_permission(id, player_name, "set_rank") then
+				ctx.page = "info_form"
+				ctx.info = {
+					msg = "Insufficient privileges",
+					status = true,
+					color = "red",
+					bold = true
+				}
+				return true, true
+			end
+
+			if ctx.rank_selected and ctx.playerlist_selected then
+				local selected_name = ctx.playerlist_selected
+				if selected_name == player_name then
+					return true
+				end
+
+				local rank_name = ctx.ranklist[ctx.rank_selected]
+
+				if rank_name == "owner" then
+					ctx.set_rank_error = "The rank cannot be changed to \"Owner\""
+					return true, true
+				end
+
+				ctx.page = "info_form"
+				if ctf_clans.set_member_rank(id, selected_name, rank_name) then
+					ctx.ranklist = nil
+					ctx.info = {
+						msg = selected_name  .. "'s rank has been changed to " .. rank_name,
+						status = true,
+						bold = true
+					}
+				else
+					ctx.info = {
+						msg = "Failed to change",
+						status = false,
+						bold = true
+					}
+				end
+			end
+			update_fs = true
+
+		elseif fields.kick then
+			if not ctf_clans.has_permission(id, player_name, "kick") then
+				ctx.page = "info_form"
+				ctx.info = {
+					msg = "Insufficient privileges",
+					status = true,
+					color = "red",
+					bold = true
+				}
+				return true, true
+			end
+
+			local selected_name = ctx.playerlist_selected
+			if selected_name then
+				ctx.page = "question_form"
+				ctx.question = {
+					msg = "Kick " .. selected_name.. "?",
+					bold = true,
+
+					callback_yes = function(player, ctx)
+						if ctf_clans.remove_member(id, selected_name) then
+							ctx.page = "info_form"
+							ctx.info = {
+								msg = "The player " .. selected_name .. " has been kicked",
+								status = true,
+								bold = true
+							}
+							minetest.chat_send_player(selected_name, "You are no longer a member of the clan " .. clan_def.clan_name)
+						end
+						return true
+					end,
+
+					callback_no = function(player, ctx)
+						ctx.page = "clan_manager"
+						return true
+					end,
+				}
+			end
+			update_fs = true
+
+		elseif fields.question_yes then
+			local question = ctx.question
+			if question.callback_yes then
+				update_fs = question.callback_yes(player, ctx)
+				question = nil
+			end
+		elseif fields.question_no then
+			local question = ctx.question
+			if question.callback_no then
+				update_fs = question.callback_no(player, ctx)
+				question = nil
+			end
+		end
+
+
+		return true, update_fs
+	end,
+
+	on_enter = function(self, player, context)
+		ui_data[player:get_player_name()] = {}
+	end,
+
+	on_leave = function(self, player, context)
+		ui_data[player:get_player_name()] = nil
+	end
+})
---
+++ mods/ctf/ctf_clans/init.lua
@@ -0,0 +1,366 @@
+-- * Copyright (c) 2024 Nanowolf4 (n4w@tutanota.com)
+-- * SPDX-License-Identifier: GPL-3.0-or-later
+
+local storage = minetest.get_mod_storage()
+
+ctf_clans = {
+	clans_ranking = {},
+	max_id = 0,
+	registered_clans = {},
+}
+
+local default_rank_icons = {
+	"ctf_clans_owner.png",
+	"ctf_clans_member.png"
+}
+
+local permissions = {
+	owner = {"any"},
+	mod = {"invite", "kick", "set_rank"},
+	member = {"invite"},
+	guest = {}
+}
+
+local clans_data = {}
+
+local clans_data_key = "clan_data:%d"
+local registered_clans_key = "registered_clans"
+local player_clan_id_key = "clan_id:"
+
+local function get_clan_data(id)
+	local this_clan = clans_data[id]
+	if this_clan then
+		return this_clan
+	end
+	local key = string.format(clans_data_key, id)
+	if storage:contains(key) then
+		-- minetest.log("action", "Loading clan data of " .. id .. " from modstorage")
+		return minetest.deserialize(storage:get(key))
+	end
+	return nil
+end
+
+local function load_clan_data(id)
+	local data = get_clan_data(id)
+	if data ~= clans_data[id] then
+		clans_data[id] = data
+		minetest.log("action", "Clan data loaded: " .. id)
+	elseif not data then
+		return false
+	end
+	return true
+end
+
+ctf_clans.load_clan_data = load_clan_data
+
+local function save_clan_data(id)
+	local key = string.format(clans_data_key, id)
+	local this_clan = clans_data[id]
+	if this_clan then
+		local serialized_data = minetest.serialize(this_clan)
+		storage:set_string(key, serialized_data)
+		minetest.debug("Saved clan data for: " .. id)
+		return true
+	end
+	return false
+end
+
+local function delete_clan_data(id)
+	storage:set_string(string.format(clans_data_key, id), nil)
+end
+
+local function save_registered_clans()
+	local serialized_data = minetest.serialize(ctf_clans.registered_clans)
+	storage:set_string(registered_clans_key, serialized_data)
+end
+
+function ctf_clans.get_clan_def(id)
+	if load_clan_data(id) then
+		return clans_data[id]
+	end
+	return nil
+end
+
+function ctf_clans.get_clan_name(id)
+	local def = clans_data[id]
+	return def.clan_name
+end
+
+function ctf_clans.is_clan_exist(id)
+	if id and load_clan_data(id) and clans_data[id].clan_name then
+		return true
+	end
+	return false
+end
+
+function ctf_clans.get_clan_id(player_name)
+	local key = player_clan_id_key .. player_name
+	if storage:contains(key) then
+		local id = storage:get_int(key)
+		if id > 0 then
+			return id
+		end
+	end
+end
+
+function ctf_clans.is_any_members_online(id)
+	if not clans_data[id] then
+		return false
+	end
+
+	local members = clans_data[id].members
+	for pn in pairs(members) do
+		if minetest.get_player_by_name(pn) then
+			return true
+		end
+	end
+
+	return false
+end
+
+function ctf_clans.get_member_rank(id, player_name)
+	local this_clan = clans_data[id]
+	local member_def = this_clan.members[player_name]
+	if member_def then
+		return member_def.rank
+	end
+end
+
+local function check_default_permissions(rank, action)
+    local rank_permissions = permissions[rank]
+    if not rank_permissions then
+        return false
+    end
+
+    if rank_permissions[1] == "any" then
+        return true
+    end
+
+    for _, permission in ipairs(rank_permissions) do
+        if permission == action then
+            return true
+        end
+    end
+
+    return false
+end
+
+function ctf_clans.has_permission(id, player_name, action)
+	local this_clan = clans_data[id]
+
+	local rank_name = this_clan.members[player_name].rank
+	if this_clan.owner == player_name then
+		return true
+	end
+
+	if check_default_permissions(rank_name, action) then
+		return true
+	end
+
+	local rank_def = this_clan.ranks[rank_name]
+
+	if rank_def.permissions then
+		for _, permission in ipairs(rank_def.permissions) do
+			if permission == action then
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
+function ctf_clans.get_rank_icon(rank_def)
+	if rank_def.icon_index then
+		return default_rank_icons[rank_def.icon_index]
+	elseif rank_def.icon then
+		return rank_def.icon
+	end
+end
+
+function ctf_clans.set_member_rank(id, member_name, new_rank)
+	local this_clan = clans_data[id]
+	local member_def = this_clan.members[member_name]
+	local rank_def = this_clan.ranks[new_rank]
+	if member_def and rank_def then
+		-- if new_rank == "owner" then
+		-- 	this_clan.owner = member_name
+		-- end
+		member_def.rank = new_rank
+		return true
+	end
+
+	return false
+end
+
+function ctf_clans.create(player_name, def)
+	local fields = {
+		clan_name = def.clan_name or "Unknown",
+		color = def.color or "FFFFFF",
+		title = def.title,
+		description = def.description or "",
+		owner = def.owner,
+		creation_time = os.time(),
+		members = {[def.owner] = {rank = "owner"}},
+		ranks = {
+			owner = {icon_index = 1, permissions = {}},
+			member = {icon_index = 2, permissions = {}},
+			custom = {icon = "ctf_clans_random_color.png"},
+			custom_test2 = {icon = "default_dirt.png"},
+			guest = {}
+		},
+	}
+
+	local new_id = ctf_clans.max_id + 1
+	storage:set_int("max_id", new_id)
+	ctf_clans.max_id = new_id
+
+	clans_data[new_id] = fields
+	table.insert(ctf_clans.registered_clans, new_id)
+	save_clan_data(new_id)
+	save_registered_clans()
+
+	local key = player_clan_id_key .. player_name
+	storage:set_int(key, new_id)
+
+	minetest.debug("Player " .. def.owner .. " created a new clan: " .. def.clan_name .. " ID:" .. new_id)
+
+	return new_id
+end
+
+function ctf_clans.remove_clan(id)
+	for pn in pairs(clans_data[id].members) do
+		local key = player_clan_id_key .. pn
+		if storage:contains(key) then
+			storage:set_int(key, 0)
+			minetest.debug("Set clan_member to 0 of " .. pn)
+		end
+	end
+
+	local regid = table.indexof(ctf_clans.registered_clans, id)
+	if regid ~= -1 then
+		table.remove(ctf_clans.registered_clans, regid)
+		minetest.debug("Removed " .. regid .. " from the list of the registered clans")
+	end
+
+	clans_data[id] = nil
+	delete_clan_data(id)
+	save_registered_clans()
+
+	minetest.debug("Clan " .. id .. " has been removed")
+end
+
+function ctf_clans.add_member(id, player_name)
+	local this_clan = clans_data[id]
+	if ctf_clans.get_clan_member(id, player_name) then
+		minetest.debug("already member of the clan")
+		return false
+	end
+	this_clan.members[player_name] = {rank = "member"}
+	save_clan_data(id)
+	local key = player_clan_id_key .. player_name
+	storage:set_int(key, id)
+	minetest.debug(player_name .. " joined the clan " .. this_clan.clan_name)
+	return true
+end
+
+function ctf_clans.get_clan_member(id, player_name)
+	local this_clan = ctf_clans.get_clan_def(id)
+	if this_clan then
+		if this_clan.members[player_name] then
+			return this_clan.members[player_name]
+		end
+	end
+end
+
+function ctf_clans.remove_member(id, player_name)
+	local this_clan = clans_data[id]
+	if this_clan.owner == player_name then
+		minetest.log("warning", "Clan owner attempt to leave own clan")
+		return false
+	end
+
+	if this_clan.members[player_name] then
+		this_clan.members[player_name] = nil
+		save_clan_data(id)
+		local key = player_clan_id_key .. player_name
+		storage:set_int(key, 0)
+		minetest.debug(player_name .. " left the clan " .. this_clan.clan_name)
+		return true
+	end
+	return false
+end
+
+function ctf_clans.fix_entry(player_name)
+	local id = ctf_clans.get_clan_id(player_name)
+	if id and not load_clan_data(id) then
+		local key = player_clan_id_key .. player_name
+		if storage:contains(key) then
+			storage:set_int(key, 0)
+			minetest.log("action", "removing non-existent clan id of " .. player_name)
+			return true
+		end
+	end
+	return false
+end
+
+function ctf_clans.get_chat_prefix(player_name)
+	local id = ctf_clans.get_clan_id(player_name)
+	if id and ctf_clans.is_clan_exist(id) then
+		local this_clan = clans_data[id]
+		local prefix = this_clan.prefix or this_clan.clan_name
+		return {
+			prefix = "[" .. prefix .. "]",
+			color = this_clan.color
+		}
+	end
+end
+
+minetest.register_on_joinplayer(function(player)
+	local player_name = player:get_player_name()
+	local id = ctf_clans.get_clan_id(player_name)
+	if not id then
+		minetest.debug(player_name .. "(id) is not in the clan")
+		return
+	end
+	if ctf_clans.is_clan_exist(id) then -- Load the clan data
+		minetest.debug("clan data loaded: " .. id)
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local id = ctf_clans.get_clan_id(player:get_player_name())
+	if id and id > 0 then
+		save_clan_data(id)
+		if not ctf_clans.is_any_members_online(id) then
+			clans_data[id] = nil
+			minetest.log("action", "Unloaded clan data ID: " .. id)
+		end
+	end
+end)
+
+minetest.register_on_mods_loaded(function()
+	local max_id_key = "max_id"
+	if storage:contains(max_id_key) then
+		ctf_clans.max_id = storage:get_int(max_id_key)
+	end
+
+	local rc_key = registered_clans_key
+	if storage:contains(rc_key) then
+		ctf_clans.registered_clans = minetest.deserialize(storage:get(rc_key))
+	end
+end)
+
+minetest.register_on_shutdown(function()
+	for id, _ in pairs(clans_data) do
+		if id > 0 then
+			save_clan_data(id)
+		end
+	end
+end)
+
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+dofile(modpath .. "/chat_commands.lua")
+dofile(modpath .. "/invitation.lua")
+dofile(modpath .. "/clan_creator.lua")
+dofile(modpath .. "/clan_ui.lua")
